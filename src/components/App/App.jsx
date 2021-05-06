acfd416f2d034d230e0328abe192d110da7b91e8
Karsten Jepsen <Kjepsen86@gmail.com>
2021-05-06 11:51:48 -0500

    basic set up SELECT PIZZA

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import SelectPizza from '../SelectPizza/SelectPizza';
 
 function App() {
   
@@ -10,7 +11,8 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      
+      <SelectPizza />
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
