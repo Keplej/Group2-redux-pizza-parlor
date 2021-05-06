e7f19d2779df68239275faaae5ff1cbfea7b06fd
Johnny Keple <Johnny.Keple@gmail.com>
2021-05-06 11:36:56 -0500

    GET ROUTE

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchPizza();
+  }, [])
+
+  const fetchPizza = () => {
+    axios.get('/api/pizza')
+    .then(response => {
+      console.log(response.data);
+      dispatch({
+        type: 'GET_PIZZA_LIST',
+        payload: response.data});
+    })
+    .catch(error => {
+      alert(`Sorry, things are not currently working at the moment... Try again later!`);
+      console.log('Error getting pizza', error);
+    })
+  }
 
   return (
     <div className='App'>
