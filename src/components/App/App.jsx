3bc260cb46d2ea4c499e2d6ddbaed67922674e66
Raveryk <raveryk@gmail.com>
2021-05-06 11:44:27 -0500

    Added PizzaCheckout component and installed react-redux and redux.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import PizzaCheckout from '../PizzaCheckout/PizzaCheckout';
 
 function App() {
   
@@ -13,6 +14,9 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+
+
+      <PizzaCheckout />
   
     </div>
   );
