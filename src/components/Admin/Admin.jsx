8f0afc6a442e408ffcb8acf246dd1e1f2e3a494a
Johnny Keple <Johnny.Keple@gmail.com>
2021-05-06 14:11:39 -0500

    Adding in Admin

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,24 @@
+
+function Admin() {
+
+
+
+    return(
+        <div>
+            <h1>Admin</h1>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Time Order Placed</th>
+                    <th>Type</th>
+                    <th>Cost</th>
+                </tr>
+            </table>
+            <tbody>
+                {/* THIS IS WHERE THE INFO WILL SHOW UP */}
+            </tbody>
+        </div>
+    )
+}
+
+export default Admin;
